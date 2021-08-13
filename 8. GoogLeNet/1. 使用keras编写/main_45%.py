1f55c40e2415015c522d87fc36a2aa007bc14df4
Wenxiang Qian <50189695@qq.com>
2021-08-13 23:29:27 +0800

    Add files via upload

---
+++ 8. GoogLeNet/1. 使用keras编写/main_45%.py
@@ -0,0 +1,108 @@
+import tensorflow.compat.v1 as tf
+import tensorflow.keras as keras
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, Dropout, Flatten
+from tensorflow.keras import regularizers
+from tensorflow.python.keras.layers import ZeroPadding2D, LayerNormalization
+from keras.layers.merge import concatenate
+from keras.models import Model
+
+tf.disable_v2_behavior()
+
+# label_mode: one of "fine", "coarse". If it is "fine" the category labels are the fine-grained labels,
+# if it is "coarse" the output labels are the coarse-grained superclasses. we've used fine here, so it is divided
+# into 100 categories.
+# https://keras.io/api/datasets/cifar100/
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data(label_mode="fine")
+
+
+weight_decay = 0.0013
+def inception_model(input, filters_1x1, filters_3x3_reduce, filters_3x3, filters_5x5_reduce, filters_5x5, filters_pool_proj):
+    conv_1x1 = Conv2D(filters=filters_1x1, kernel_size=(1, 1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(input)
+    conv_3x3_reduce = Conv2D(filters=filters_3x3_reduce, kernel_size=(1, 1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(input)
+    conv_3x3 = Conv2D(filters=filters_3x3, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(conv_3x3_reduce)
+    conv_5x5_reduce  = Conv2D(filters=filters_5x5_reduce, kernel_size=(1, 1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(input)
+    #changed here!!
+    conv_5x5 = Conv2D(filters=filters_5x5, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(conv_5x5_reduce)
+    maxpool = MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding='same')(input)
+    maxpool_proj = Conv2D(filters=filters_pool_proj, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(maxpool)
+    inception_output = concatenate([conv_1x1, conv_3x3, conv_5x5, maxpool_proj], axis=3)  # use tf as backend
+    return inception_output
+
+
+input = Input(shape=(32, 32, 3))
+#7x7 is too big for 32x32x3 images. change the first 2 layers to 5x5 and 2x2
+#conv1_7x7_s2 = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(input)
+
+#maxpool1_3x3_s2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(conv1_7x7_s2)
+
+#conv2_3x3_reduce = Conv2D(filters=64, kernel_size=(1, 1), padding='same', activation='relu',
+ #                         kernel_regularizer=regularizers.l2(weight_decay))(conv1_7x7_s2)
+
+#, strides=(2, 2) is added.
+conv2_3x3 = Conv2D(filters=192, kernel_size=(3, 3), strides=(2, 2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(weight_decay))(input)
+                  # )(conv2_3x3_reduce)
+
+
+maxpool2_3x3_s2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(conv2_3x3)
+
+#the network is too large for CIFAR-100. delete 2 inception models will lower the chance to overfitting.
+#inception_3a = inception_model(input=maxpool2_3x3_s2, filters_1x1=64, filters_3x3_reduce=96, filters_3x3=128,
+#                               filters_5x5_reduce=16, filters_5x5=32, filters_pool_proj=32)
+
+#inception_3b = inception_model(input=inception_3a, filters_1x1=128, filters_3x3_reduce=128, filters_3x3=192,
+#                               filters_5x5_reduce=32, filters_5x5=96, filters_pool_proj=64)
+
+maxpool3_3x3_s2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(maxpool2_3x3_s2)
+
+inception_4a = inception_model(input=maxpool3_3x3_s2, filters_1x1=192, filters_3x3_reduce=96, filters_3x3=208,
+                               filters_5x5_reduce=16, filters_5x5=48, filters_pool_proj=64)
+
+inception_4b = inception_model(input=inception_4a, filters_1x1=160, filters_3x3_reduce=112, filters_3x3=224,
+                               filters_5x5_reduce=24, filters_5x5=64, filters_pool_proj=64)
+
+inception_4c = inception_model(input=inception_4b, filters_1x1=128, filters_3x3_reduce=128, filters_3x3=256,
+                               filters_5x5_reduce=24, filters_5x5=64, filters_pool_proj=64)
+
+inception_4d = inception_model(input=inception_4c, filters_1x1=112, filters_3x3_reduce=144, filters_3x3=288,
+                               filters_5x5_reduce=32, filters_5x5=64, filters_pool_proj=64)
+
+inception_4e = inception_model(input=inception_4d, filters_1x1=256, filters_3x3_reduce=160, filters_3x3=320,
+                               filters_5x5_reduce=32, filters_5x5=128, filters_pool_proj=128)
+
+maxpool4_3x3_s2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(inception_4e)
+
+inception_5a = inception_model(input=maxpool4_3x3_s2, filters_1x1=256, filters_3x3_reduce=160, filters_3x3=320,
+                               filters_5x5_reduce=32, filters_5x5=128, filters_pool_proj=128)
+
+inception_5b = inception_model(input=inception_5a, filters_1x1=384, filters_3x3_reduce=192, filters_3x3=384,
+                               filters_5x5_reduce=48, filters_5x5=128, filters_pool_proj=128)
+
+#don't change 7x7 to 2x2, 7x7 will keep the output as original if it is smaller than 7x7.
+#but if we set it to 2x2, it will /2 almost everytime, it lowers the output, and the output will be as small
+#as 2x2 or even 1x1, bad for us.
+averagepool1_7x7_s1 = AveragePooling2D(pool_size=(7, 7), strides=(2, 2), padding='same')(inception_5b)
+
+#drop1 = Dropout(rate=0.4)(averagepool1_7x7_s1)
+#flatten = Flatten()(drop1)
+flatten = Flatten()(averagepool1_7x7_s1)
+
+linear = Dense(units=100, activation='softmax', kernel_regularizer=regularizers.l2(weight_decay))(flatten)
+last = linear
+
+model = Model(inputs=input, outputs=last)
+model.summary()
+BATCH_SIZE = 64
+EPOCH = 100
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+#model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCH, validation_data=(x_test, y_test), shuffle=True)
+
+#no good here..
+datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+datagen.fit(x_train)
+
+# using processed image data to train the model
+model.fit_generator(datagen.flow(x_train, y_train, batch_size=BATCH_SIZE), epochs=EPOCH, validation_data=(x_test, y_test))
---
+++ 8. GoogLeNet/2. 使用底层库编写/main.py
@@ -0,0 +1,129 @@
+import tensorflow.compat.v1 as tf
+import tensorflow.keras as keras
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, Dropout, Flatten
+from tensorflow.keras import regularizers
+from tensorflow.python.keras.layers import ZeroPadding2D, LayerNormalization
+from keras.layers.merge import concatenate
+from keras.models import Model
+
+import nnlayers
+
+tf.disable_v2_behavior()
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# label_mode: one of "fine", "coarse". If it is "fine" the category labels are the fine-grained labels,
+# if it is "coarse" the output labels are the coarse-grained superclasses. we've used fine here, so it is divided
+# into 100 categories.
+# https://keras.io/api/datasets/cifar100/
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data(label_mode="fine")
+
+y_train = keras.utils.to_categorical(y_train, 100)
+y_test = keras.utils.to_categorical(y_test, 100)
+
+#input = Input(shape=(32, 32, 3))
+
+IMAGE_WIDTH = 32
+IMAGE_DEPTH = 3
+LABEL_BYTES = 1
+IMAGE_SIZE = IMAGE_WIDTH * IMAGE_WIDTH * IMAGE_DEPTH  # 3072
+
+x = tf.compat.v1.placeholder(tf.float32, shape=[None, IMAGE_SIZE])
+y_ = tf.compat.v1.placeholder(tf.float32, shape=[None, 100])
+x_image = tf.reshape(x, [-1, 32, 32, 3])
+
+
+def inception(input, f1, f2, f3, f4 ,f5 ,f6):
+    inception_1x1 = nnlayers.conv_2d(input, filter_count=f1, kernel_size=1, strides=1)
+    inception_3x3_reduce = nnlayers.conv_2d(input, filter_count=f2, kernel_size=1, strides=1)
+    inception_3x3 = nnlayers.conv_2d(inception_3x3_reduce, filter_count=f3, kernel_size=3, strides=1)
+    inception_5x5_reduce = nnlayers.conv_2d(input, filter_count=f4, kernel_size=1, strides=1)
+    inception_5x5 = nnlayers.conv_2d(inception_5x5_reduce, filter_count=f5, kernel_size=5, strides=1)
+    inception_pool = nnlayers.max_pooling(input, 3, 1)
+    inception_pool_proj = nnlayers.conv_2d(inception_pool, filter_count=f6, kernel_size=1, strides=1)
+    output = tf.concat([inception_1x1, inception_3x3, inception_5x5,inception_pool_proj],axis=3)
+    return output
+
+def build_model():
+    conv1_7x7_s2 = nnlayers.conv_2d(x_image, filter_count=64, kernel_size=7, strides=1) #2)
+    #pool1_3x3_s2 = max_pool('pool1_3x3_s2', conv1_7x7_s2, 3, 2)
+    #pool1_norm1 = lrn('pool1_norm1', pool1_3x3_s2)
+    conv2_3x3_reduce = nnlayers.conv_2d(conv1_7x7_s2, filter_count=64, kernel_size=1, strides=1)
+    conv2_3x3 = nnlayers.conv_2d(conv2_3x3_reduce, filter_count=192, kernel_size=3, strides=1)
+    conv2_norm2 = nnlayers.lrn(conv2_3x3)
+    pool2_3x3_s2 = nnlayers.max_pooling(conv2_norm2, 3, 2)
+
+    inception_3a_output = inception(pool2_3x3_s2, 64, 96, 128, 16, 32, 32)
+
+    inception_3b_output = inception(inception_3a_output, 128, 128, 192, 32, 96, 64)
+
+    pool3_3x3_s2 = nnlayers.max_pooling(inception_3b_output, 3, 2)
+
+    inception_4a_output = inception(pool3_3x3_s2, 192, 96, 208, 16, 48, 64)
+
+    inception_4b_output = inception(inception_4a_output, 160, 112, 224, 24, 64, 64)
+
+    inception_4c_output = inception(inception_4b_output, 128, 128, 256, 24, 64, 64)
+
+    inception_4d_output = inception(inception_4c_output, 112, 144, 288, 32, 64, 64)
+
+    inception_4e_output = inception(inception_4d_output, 256, 160, 320, 32, 128, 128)
+
+    pool4_3x3_s2 = nnlayers.max_pooling(inception_4e_output, 3, 2)
+
+    inception_5a_output = inception(pool4_3x3_s2, 256, 160, 320, 32, 128, 128)
+
+    inception_5b_output = inception(inception_5a_output, 384, 192, 384, 48, 128, 128)
+
+    pool5_7x7_s1 = nnlayers.avg_pooling(inception_5b_output, 7, 1)
+    pool5_drop_7x7_s1 = nnlayers.dropout(pool5_7x7_s1, 0.6)
+
+    #logits = fc('loss3_classifier', pool5_drop_7x7_s1, out_nodes=100)
+    logits = nnlayers.dense(pool5_drop_7x7_s1, 100, kernel_regularizer='none', activation='relu')
+    return logits
+
+k = build_model()
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=k))
+train_step = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cross_entropy)
+
+#x_train = x_train.astype("float32")
+#x_test = x_test.astype("float32")
+#x_train /= 255.0
+#x_test /= 255.0
+
+sess = tf.InteractiveSession()
+#tf.global_variables_initializer().run()
+
+correct_prediction = tf.equal(tf.argmax(k, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+x_train = x_train.reshape(x_train.shape[0], IMAGE_SIZE)
+x_test = x_test.reshape(x_test.shape[0], IMAGE_SIZE)
+
+BATCH_SIZE = 128
+TRAINING_DATASET_COUNT = 50000
+# train 100 rounds on every dataset in training set.
+EPOCHES = 100
+BATCHES = (TRAINING_DATASET_COUNT // BATCH_SIZE) * EPOCHES
+
+(x_small_test_batch, y_small_test_batch) = nnlayers.get_batch(x_test, y_test, 100, 1, 10000)
+
+with tf.Session() as tfsess:
+    tf.global_variables_initializer().run()
+    for i in range(BATCHES):
+        (x_batch, y_batch) = nnlayers.get_batch(x_train, y_train, BATCH_SIZE, i, 50000)
+        _, loss_value = tfsess.run([train_step, cross_entropy], feed_dict={x: x_batch, y_: y_batch})
+        #report loss every 10 batches
+        if i % 10 == 0:
+            print("batch %d, loss: %g" % (i, loss_value))
+
+        #test after a whole epoch
+        #if i > 0 and i % (TRAINING_DATASET_COUNT // BATCH_SIZE) == 0:
+        #    train_accuracy = accuracy.eval(feed_dict={x: x_small_test_batch, y_: y_small_test_batch})
+        #    print("batch %d, loss: %g, acc: %g" % (i, loss_value, train_accuracy))
+
+    train_accuracy = accuracy.eval(feed_dict={x: x_train, y_: y_test})
+    print("batch final, acc: %g" % train_accuracy)
